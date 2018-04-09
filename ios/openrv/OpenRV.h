2b7df037f99308ac05ea2ac8923032b2d64a7f88
christoph <christoph@moede.info>
2018-04-09 15:38:39 +0200

    Add framebuffer access and events to iOS framwork

--- ios/openrv/OpenRV.h
+++ ios/openrv/OpenRV.h
@@ -14,23 +14,38 @@ FOUNDATION_EXPORT const unsigned char openrvVersionString[];
 @class OpenRVContext;
 @class OpenRVFramebufferUpdate;
 
+@interface OpenRVFramebuffer : NSObject
+@property (nonatomic, readonly) uint32_t width;
+@property (nonatomic, readonly) uint32_t height;
+@property (nonatomic, readonly) uint32_t bitsPerPixel;
+@property (nonatomic, readonly) const uint8_t* buffer;
+@end
 
 @protocol OpenRVContextDelegate<NSObject>
 @optional
-    -(void) contextDidConnect:(OpenRVContext*)context withError:(const orv_error_t*)error;
-    -(void) contextDidDisconnected:(OpenRVContext*)context;
-    -(void) contextFramebufferUpdated:(OpenRVContext*)context frame:(CGRect)frame;
+    -(void) openRVContextDidConnect:(OpenRVContext*)context;
+    -(void) openRVContextConnectFailed:(OpenRVContext*)context withError:(const orv_error_t*)error;
+    -(void) openRVContextDidDisconnected:(OpenRVContext*)context;
+    -(void) openRVContextFramebufferUpdated:(OpenRVContext*)context frame:(CGRect)frame;
+    -(void) openRVContextFramebufferUpdateFinished:(OpenRVContext*)context;
 @end
 
 @interface OpenRVContext : NSObject
 -(instancetype) init;
--(void) connectToHost:(NSString*)host password:(NSString*)password;
+-(void) setCredentials:(nullable NSString*)username password:(nullable NSString*)password;
+-(void) connectToHost:(nonnull NSString*)host;
 -(void) disconnect;
--(BOOL) lockFramebuffer;
+
+// frame buffer access
+-(OpenRVFramebuffer*) lockFramebuffer;
 -(void) unlockFramebuffer;
+
+-(void) sendKeyEvent:(uint32_t)keyCode isDown:(BOOL)down;
+-(void) sendPointerEvent:(int32_t)x y:(int32_t)y buttonMask:(uint32_t)buttonMask;
 
 @property(nonatomic, assign) uint16_t port;
 @property(nonatomic, assign) BOOL viewOnly;
 @property(nonatomic, assign) CGSize framebufferSize;
+@property(nonatomic, assign) orv_auth_type_t authType;
 @property(nullable,nonatomic,weak) id<OpenRVContextDelegate> delegate;
 @end
--- ios/openrv/OpenRV.m
+++ ios/openrv/OpenRV.m
@@ -3,19 +3,45 @@
 
 static void orvEventCallback(struct orv_context_t* ctx, orv_event_t* event);
 
+@implementation OpenRVFramebuffer
+{
+    const orv_framebuffer_t* mFramebuffer;
+}
+-(uint32_t) width
+{
+    return mFramebuffer->mWidth;
+}
+-(uint32_t) height
+{
+    return mFramebuffer->mHeight;
+}
+-(uint32_t) bitsPerPixel
+{
+    return mFramebuffer->mBitsPerPixel;
+}
+-(const uint8_t*) buffer
+{
+    return mFramebuffer->mFramebuffer;
+}
+-(void) setFramebuffer:(const orv_framebuffer_t*)fb
+{
+    mFramebuffer = fb;
+}
+@end
 
 @implementation OpenRVContext
 {
     orv_context_t* mContext;
-    const orv_framebuffer_t* mFramebuffer;
+    OpenRVFramebuffer* mFramebuffer;
 }
 -(instancetype) init
 {
     self = [super init];
     if (self) {
-        self.port = 9999;
+        self.port = 38973;
         self.viewOnly = YES;
         self.framebufferSize = CGSizeMake(0, 0);
+        mFramebuffer = [[OpenRVFramebuffer alloc] init];
         [self createContext];
     }
     return self;
@@ -32,30 +58,32 @@ static void orvEventCallback(struct orv_context_t* ctx, orv_event_t* event);
     config.mUserData[0] = (__bridge void*)self;
     mContext = orv_init(&config);
 }
--(void) connectToHost:(NSString*)host password:(NSString*)password
+-(void) setCredentials:(NSString *)username password:(NSString *)password
+{
+    int res = orv_set_credentials(mContext, username ? username.UTF8String:NULL, password ? password.UTF8String:NULL);
+}
+-(void) connectToHost:(NSString*)host
 {
     orv_connect_options_t conOptions;
     orv_connect_options_default(&conOptions);
     conOptions.mViewOnly = self.viewOnly ? false:true;
     conOptions.mCommunicationQualityProfile = ORV_COMM_QUALITY_PROFILE_BEST; // fixme
     orv_error_t err;
-    int res = orv_connect(mContext, host.UTF8String, self.port, password.UTF8String, &conOptions, &err);
+    int res = orv_connect(mContext, host.UTF8String, self.port, &conOptions, &err);
 }
 -(void) disconnect
 {
     orv_disconnect(mContext);
 }
--(BOOL) lockFramebuffer
+-(OpenRVFramebuffer*) lockFramebuffer
 {
-    if (mFramebuffer) {
-        return YES;
+    const orv_framebuffer_t* fb = orv_acquire_framebuffer(mContext);
+    if (!fb) {
+        return nil;
     }
-    mFramebuffer = orv_acquire_framebuffer(mContext);
-    if (!mFramebuffer) {
-        return NO;
-    }
-    self.framebufferSize = CGSizeMake(mFramebuffer->mWidth, mFramebuffer->mHeight);
-    return YES;
+    self.framebufferSize = CGSizeMake(fb->mWidth, fb->mHeight);
+    [mFramebuffer setFramebuffer:fb];
+    return mFramebuffer;
 }
 -(void) unlockFramebuffer
 {
@@ -63,6 +91,14 @@ static void orvEventCallback(struct orv_context_t* ctx, orv_event_t* event);
         return;
     }
     orv_release_framebuffer(mContext);
+}
+-(void) sendKeyEvent:(uint32_t)keyCode isDown:(BOOL)down
+{
+    orv_send_key_event(mContext, down, keyCode);
+}
+-(void) sendPointerEvent:(int32_t)x y:(int32_t)y buttonMask:(uint32_t)buttonMask;
+{
+    orv_send_pointer_event(mContext, x, y, buttonMask);
 }
 
 -(void) handleEvent:(orv_event_t*)event
@@ -74,15 +110,23 @@ static void orvEventCallback(struct orv_context_t* ctx, orv_event_t* event);
         {
             const orv_connect_result_t* connectResult = (const orv_connect_result_t*)event->mEventData;
             self.framebufferSize = CGSizeMake(connectResult->mFramebufferWidth, connectResult->mFramebufferHeight);
-            if ([delegate respondsToSelector:@selector(contextDidConnect:withError:)]) {
-                [delegate contextDidConnect:self withError:&connectResult->mError];
+            self.authType = connectResult->mAuthenticationType;
+            if (!connectResult->mError.mHasError) {
+                if ([delegate respondsToSelector:@selector(openRVContextDidConnect:)]) {
+                    [delegate openRVContextDidConnect:self];
+                }
+                orv_request_framebuffer_update(mContext, 0, 0, self.framebufferSize.width, self.framebufferSize.height);
+            } else {
+                if ([delegate respondsToSelector:@selector(openRVContextConnectFailed:withError:)]) {
+                    [delegate openRVContextConnectFailed:self withError:&connectResult->mError];
+                }
             }
         }
         break;
     case ORV_EVENT_DISCONNECTED:
         {
-            if ([delegate respondsToSelector:@selector(contextDidDisconnected:)]) {
-                [delegate contextDidDisconnected:self];
+            if ([delegate respondsToSelector:@selector(openRVContextDidDisconnected:)]) {
+                [delegate openRVContextDidDisconnected:self];
             }
         }
         break;
@@ -90,13 +134,29 @@ static void orvEventCallback(struct orv_context_t* ctx, orv_event_t* event);
         {
             orv_event_framebuffer_t* fb = event->mEventData;
             CGRect rect = CGRectMake(fb->mX, fb->mY, fb->mWidth, fb->mHeight);
-            if ([delegate respondsToSelector:@selector(contextFramebufferUpdated:frame:)]) {
-                [delegate contextFramebufferUpdated:self frame:rect];
+            if ([delegate respondsToSelector:@selector(openRVContextFramebufferUpdated:frame:)]) {
+                [delegate openRVContextFramebufferUpdated:self frame:rect];
             }
         }
         break;
+    case ORV_EVENT_FRAMEBUFFER_UPDATE_REQUEST_FINISHED:
+        {
+            if ([delegate respondsToSelector:@selector(openRVContextFramebufferUpdateFinished:)]) {
+                [delegate openRVContextFramebufferUpdateFinished:self];
+            }
+            orv_request_framebuffer_update(mContext, 0, 0, self.framebufferSize.width, self.framebufferSize.height);
+        }
+        break;
+    case ORV_EVENT_NONE:
+    case ORV_EVENT_CUT_TEXT:
+    case ORV_EVENT_CURSOR_UPDATED:
+    case ORV_EVENT_BELL:
+    case ORV_EVENT_THREAD_STARTED:
+    case ORV_EVENT_THREAD_ABOUT_TO_STOP:
+        break;
     }
     orv_event_destroy(event);
+
 }
 @end
 
